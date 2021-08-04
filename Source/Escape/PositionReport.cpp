6fd908a65c6dc2e7c4bff339d1c509eae5f4c67d
Ryzzen <a.mayaud@hotmail.fr>
2021-08-04 11:47:24 +0200

    Position Report added

---
+++ Source/Escape/PositionReport.cpp
@@ -0,0 +1,37 @@
+// Copyright Antoine Mayaud 2021.
+
+
+#include "PositionReport.h"
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString Name = GetOwner()->GetName();
+	FString Location = GetOwner()->GetActorLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *Name, *Location);
+	
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
